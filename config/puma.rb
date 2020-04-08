a63233bc82665a1191448bae80d6ff6c28f910b3
Alex <AlexAylwin@gmail.com>
2020-04-08 08:00:52 -0400

    Configures project to run on a limited heroku
    Added heroku orb to circleci config
    Removes clustered mode and switches to single thread

--- config/puma.rb
+++ config/puma.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
-threads threads_count, threads_count
+#workers 1#Integer(ENV['WEB_CONCURRENCY'] || 1)
+#threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 2)
+threads 1, 1#threads_count, threads_count
 
-preload_app!
+# preload_app!
 
 rackup      DefaultRackup
 port        ENV['PORT']     || 3000
